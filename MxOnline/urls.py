e0582001cb2c298a15b65073f29684c529ce81d0
virlaser <liziyan784617756@foxmail.com>
2017-05-29 14:32:13 +0800

    主要是增加了用户注册和登录的功能，期中大部分是对邮箱的操作。前端给的页面代码中有很多需要修改的。

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 """MxOnline URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,16 +15,31 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 
-from Users.views import user_login
-
+from Users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, ModifyPwdView
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
 
     url('^$', TemplateView.as_view(template_name="index.html"), name="index"),
-    url('^login/$', user_login, name="user_login")
+    # 这里调用的不是as_view这个句柄,所以要加上()
+    url('^login/$', LoginView.as_view(), name="user_login"),
+    url('^register/$', RegisterView.as_view(), name="register"),
+
+    # 验证码配置
+    url(r'^captcha/', include('captcha.urls')),
+
+    # 用户激活
+    url(r'^active/(?P<active_code>.*)/$', ActiveUserView.as_view(), name="user_active"),
+
+    # 找回密码
+    url(r'^forget/$', ForgetPwdView.as_view(), name="forget_pwd"),
+
+    # 重置密码链接
+    url(r'^reset/(?P<active_code>.*)/$', ResetView.as_view(), name="reset_pwd"),
+    url(r'^modify_pwd/$', ModifyPwdView.as_view(), name="modify_pwd")
+
 ]
--- apps/Users/views.py
+++ apps/Users/views.py
@@ -1,35 +1,140 @@
-#coding:utf-8
+# coding:utf-8
 
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
+from django.contrib.auth.hashers import make_password
 
-from .models import UserProfile
+from .models import UserProfile, EmailVerifyRecord
+from .forms import LoginFrom, RegisterForm, ForgetForm, ModifyPwdForm
+from utils.email_send import send_register_email
 
 
+# 可以通过邮件登录
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            # 通过Q参数定制数据库查询认证规则
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
             return None
 
 
-# Create your views here.
-
-
-def user_login(request):
-    if request.method == "POST":
-        user_name = request.POST.get("username", "")
-        pass_word = request.POST.get("password", "")
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, "index.html")
-        else:
-            return render(request, "login.html", {"msg":u"用户名或密码错误"})
-    elif request.method == "GET":
+# 通过view来封装页面逻辑
+class LoginView(View):
+    def get(self, request):
         return render(request, "login.html", {})
+
+    def post(self, request):
+        # 通过form来检查输入是否合法
+        login_form = LoginFrom(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get("username", "")
+            pass_word = request.POST.get("password", "")
+            # auth认证
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                # 判断是否激活
+                if user.is_active:
+                    # 根据用户信息生成session_id
+                    login(request, user)
+                    return render(request, "index.html")
+                else:
+                    return render(request, "login.html", {"msg": "用户未激活"})
+            else:
+                # 传递参数,在页面中显示错误信息
+                return render(request, "login.html", {"msg": u"用户名或密码错误", "login_from": login_form})
+        else:
+            return render(request, "login.html", {"login_from": login_form})
+
+
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, "register.html", {'register_form': register_form})
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name = request.POST.get("email", "")
+            # 判断注册邮箱是否已经存在
+            if UserProfile.objects.filter(email=user_name):
+                return render(request, "register.html", {"msg": "用户已经存在", "register_form": register_form})
+            pass_word = request.POST.get("password", "")
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            # 要等用户点击了链接之后再成为激活状态
+            user_profile.is_active = False
+            # 使用Django自带功能对密码进行加密
+            user_profile.password = make_password(pass_word)
+            user_profile.save()
+
+            send_register_email(user_name, "register")
+            return render(request, "login.html")
+        else:
+            return render(request, "register.html", {"register_form": register_form})
+
+
+class ActiveUserView(View):
+    def get(self, request, active_code):
+        all_records = EmailVerifyRecord.objects.filter(code=active_code)
+        if all_records:
+            for record in all_records:
+                email = record.email
+                user = UserProfile.objects.get(email=email)
+                user.is_active = True
+                user.save()
+        else:
+            return render(request, "active_fail.html")
+
+        return render(request, "login.html")
+
+
+class ForgetPwdView(View):
+    def get(self, request):
+        forget_form = ForgetForm()
+        return render(request, "forgetpwd.html", {"forget_form": forget_form})
+
+    def post(self, request):
+        forget_form = ForgetForm(request.POST)
+        if forget_form.is_valid():
+            email = request.POST.get("email", "")
+            send_register_email(email, "forget")
+            return render(request, "send_success.html")
+        else:
+            return render(request, "forgetpwd.html", {"forget_form": forget_form})
+
+
+class ResetView(View):
+    def get(self, request, active_code):
+        all_records = EmailVerifyRecord.objects.filter(code=active_code)
+        if all_records:
+            for record in all_records:
+                email = record.email
+                return render(request, "password_reset.html", {"email": email})
+        else:
+            return render(request, "active_fail.html")
+        return render(request, "login.html")
+
+
+class ModifyPwdView(View):
+    def post(self, request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            pwd1 = request.POST.get("password1", "")
+            pwd2 = request.POST.get("password2", "")
+            email = request.POST.get("email", "")
+            if pwd1 != pwd2:
+                return render(request, "password_reset.html", {"email": email, "msg": "密码不一致"})
+            user = UserProfile.objects.get(email=email)
+            user.password = make_password(pwd1)
+            user.save()
+            return render(request, "login.html")
+        else:
+            email = request.POST.get("email", "")
+            return render(request, "password_reset.html", {"email": email, "modify_form": modify_form})
